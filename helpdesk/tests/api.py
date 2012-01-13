3edfd1ea7d197f10e64bd659fed8ae78e893ce54
Ivan Giuliani <giuliani.v@gmail.com>
2012-01-13 17:50:37 +0100

    Use fixtures to load test data (user's passwords are equals to their username)

--- helpdesk/tests/api.py
+++ helpdesk/tests/api.py
@@ -1,42 +1,13 @@
-from django.utils import unittest
-from django.contrib.auth.models import User
+from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
 
-from helpdesk.models import Ticket, Queue
+from helpdesk.views import api
 
 
-class APITest(unittest.TestCase):
+class APITest(TestCase):
     urls = "helpdesk.urls"
-
-    def setUp(self):
-        "Creates some test data that can be used later on in tests"
-        self.admin_user = User.objects.create_user("admin",
-                                                   "admin@admin.com",
-                                                   "adminpwd")
-        self.admin_user.is_superuser = True
-        self.admin_user.is_staff = True
-        self.admin_user.save()
-
-        self.normal_user = User.objects.create_user("normal",
-                                                    "normal@normal.com",
-                                                    "normalpwd")
-        self.normal_user.save()
-
-        self.queue = Queue(
-            title="Queue title",
-            slug="queue",
-        )
-        self.queue.save()
-
-        self.ticket = Ticket(
-            title="Ticket title",
-            description="Ticket description",
-            queue=self.queue,
-            submitter_email="test@example.com",
-            priority=3,
-        )
-        self.ticket.save()
+    fixtures = ["test_fixtures.json"]
 
     def testOnlyPostRequestsGetAccepted(self):
         "GET requests to API must be ignored"
@@ -47,6 +18,6 @@ class APITest(unittest.TestCase):
 
         response = client.post(reverse("helpdesk_api", args=("list_queues",)), {
             "user": "admin",
-            "password": "adminpwd",
+            "password": "admin",
         })
         self.assertEqual(response.status_code, 200)
