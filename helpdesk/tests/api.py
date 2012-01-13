275b694641cd2f0473e43c7e516a658f94e620a5
Ivan Giuliani <giuliani.v@gmail.com>
2012-01-13 17:55:15 +0100

    Added tests for get_ticket API call

--- helpdesk/tests/api.py
+++ helpdesk/tests/api.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.utils import simplejson
 from django.core.urlresolvers import reverse
 
 from helpdesk.views import api
@@ -8,6 +9,16 @@ from helpdesk.views import api
 class APITest(TestCase):
     urls = "helpdesk.urls"
     fixtures = ["test_fixtures.json"]
+
+    def api_call(self, method, data):
+        "Convenience method for easier API calling"
+        client = Client()
+        post_data = {
+            "user": "admin",
+            "password": "admin",
+        }
+        post_data.update(data)
+        return client.post(reverse("helpdesk_api", args=(method, )), post_data)
 
     def testOnlyPostRequestsGetAccepted(self):
         "GET requests to API must be ignored"
@@ -21,3 +32,21 @@ class APITest(TestCase):
             "password": "admin",
         })
         self.assertEqual(response.status_code, 200)
+
+    def testGetTicket(self):
+        "Test get_ticket API method"
+        non_existent_ticket = self.api_call("get_ticket", { "ticket": 25 })
+        self.assertEqual(non_existent_ticket.status_code,
+                         api.STATUS_ERROR_NOT_FOUND,
+                         "API call didn't fail on inexisting ticket")
+
+        existing_ticket = self.api_call("get_ticket", { "ticket": 1 })
+        self.assertEqual(existing_ticket.status_code, api.STATUS_OK,
+                        "Existing ticket hasn't been returned")
+
+        ticket = simplejson.loads(existing_ticket.content)
+        self.assertIsNotNone(ticket)
+
+        self.assertEquals(ticket["description"], "ticket description")
+        self.assertEquals(ticket["queue"], 1)
+        self.assertEquals(ticket["submitter_email"], "customer@customer.com")
