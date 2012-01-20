9e7e55e4a3383b06492eaf76acf7808c2a169185
Ivan Giuliani <giuliani.v@gmail.com>
2012-01-20 17:25:37 +0100

    Test add_followup API behaviour
    add_followup contained a bug that didn't allow to create private
    followups from APIs as a critical condition was never converted to a
    correct boolean value. This patch address this problems and add tests
    for add_followup.

--- helpdesk/views/api.py
+++ helpdesk/views/api.py
@@ -293,6 +293,7 @@ class API:
 
         if public not in ['y', 'n']:
             return api_return(STATUS_ERROR, "Invalid 'public' flag")
+        public = public == 'y'
 
         if not message:
             return api_return(STATUS_ERROR, "Blank message")
@@ -307,6 +308,8 @@ class API:
 
         if public:
             f.public = True
+        else:
+            f.public = False
 
         f.save()
 
